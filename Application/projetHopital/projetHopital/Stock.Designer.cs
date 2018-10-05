bdfe8c09166bf36b35995dcc2ca79d7dc69aeec1
Tom Jablonski <tomjablonski@hotmail.fr>
2018-10-05 09:55:37 +0200

    05/10/2018

--- Application/projetHopital/projetHopital/Stock.Designer.cs
+++ Application/projetHopital/projetHopital/Stock.Designer.cs
@@ -33,15 +33,20 @@
             this.btnRetour = new System.Windows.Forms.Button();
             this.btnQuitter = new System.Windows.Forms.Button();
             this.btnAjtMedicament = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // lblStock
             // 
             this.lblStock.AutoSize = true;
-            this.lblStock.Font = new System.Drawing.Font("News Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblStock.Location = new System.Drawing.Point(296, 9);
             this.lblStock.Name = "lblStock";
-            this.lblStock.Size = new System.Drawing.Size(347, 38);
+            this.lblStock.Size = new System.Drawing.Size(353, 42);
             this.lblStock.TabIndex = 1;
             this.lblStock.Text = "Gestion des stocks";
             this.lblStock.Click += new System.EventHandler(this.lblStock_Click);
@@ -102,11 +107,51 @@
             this.btnAjtMedicament.UseVisualStyleBackColor = false;
             this.btnAjtMedicament.Click += new System.EventHandler(this.btnAjtMedicament_Click);
             // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(79, 101);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(465, 295);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nom";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 265;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Quantité";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 82;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Seuil";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 62;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Numéro";
+            this.columnHeader1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader1.Width = 50;
+            // 
             // Stock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(946, 576);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.btnAjtMedicament);
             this.Controls.Add(this.btnQuitter);
             this.Controls.Add(this.btnRetour);
@@ -127,5 +172,10 @@
         private System.Windows.Forms.Button btnRetour;
         private System.Windows.Forms.Button btnQuitter;
         private System.Windows.Forms.Button btnAjtMedicament;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
     }
 }
