2030546d213dc412b145dc298a382a2171b84a41
Tom Jablonski <tomjablonski@hotmail.fr>
2018-10-09 16:45:26 +0200

    09-10-2018
    Push 2

--- Application/projetHopital/projetHopital/Passerelle.cs
+++ Application/projetHopital/projetHopital/Passerelle.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
 namespace projetHopital
 {
     class Passerelle
     {
-
+        public void donnerConnection()
+        {
+            string chaineConnection = "Data Source=WIN-921C8FKTGAE;Initial Catalog=slam2019projetHopitalGroupe4;User ID=jablonski;Password=jablonski";
+            SqlConnection maConnexion = new SqlConnection(chaineConnection);
+            maConnexion.Open();
+        }
+        
+        public void fermerConnection()
+        {
+            maConnexion.Close();
+            Console.ReadLine();
+        }
     }
 }
