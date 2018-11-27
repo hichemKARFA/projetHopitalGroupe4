ab4ece8c6adf00bf2f40fd17ec850af793df8511
Tom Jablonski <tomjablonski@hotmail.fr>
2018-11-27 15:12:11 +0100

    push

--- Projet 2/gestionSejour/src/Controller/IndicationController.php
+++ Projet 2/gestionSejour/src/Controller/IndicationController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Medicament;
+use App\Entity\Indication;
+use App\Entity\Traitement;
 
 class IndicationController extends AbstractController
 {
     /**
      * @Route("/indication", name="indication")
      */
-    public function index()
+    public function getIndications()
     {
-        return $this->render('indication/index.html.twig', [
-            'controller_name' => 'IndicationController',
-        ]);
+        $repository=$this->getDoctrine()->getRepository(Indication::class);
+		$lesIndications=$repository->findAll();
+		return $this->render('indication/index.html.twig',['indications'=>$lesIndications,]);
     }
 }
---
+++ Projet 2/gestionSejour/src/Migrations/Version20181127140605.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181127140605 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE indication (id INT AUTO_INCREMENT NOT NULL, medicament_id INT DEFAULT NULL, traitement_id INT DEFAULT NULL, posologie VARCHAR(255) DEFAULT NULL, INDEX IDX_D15065D7AB0D61F7 (medicament_id), INDEX IDX_D15065D7DDA344B6 (traitement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE indication ADD CONSTRAINT FK_D15065D7AB0D61F7 FOREIGN KEY (medicament_id) REFERENCES medicament (id)');
+        $this->addSql('ALTER TABLE indication ADD CONSTRAINT FK_D15065D7DDA344B6 FOREIGN KEY (traitement_id) REFERENCES traitement (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE indication');
+    }
+}
