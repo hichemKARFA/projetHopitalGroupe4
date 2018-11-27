79a05974c5577cb21067e758dd6b508de5d1328a
louiscom <43175944+louiscom@users.noreply.github.com>
2018-11-27 14:30:34 +0100

    ajout Traitement
    Base de données et TraitementController

---
+++ Projet 2/gestionSejour/src/Entity/Traitement.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TraitementRepository")
+ */
+class Traitement
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateDebut;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomPatient;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getNomPatient(): ?string
+    {
+        return $this->nomPatient;
+    }
+
+    public function setNomPatient(string $nomPatient): self
+    {
+        $this->nomPatient = $nomPatient;
+
+        return $this;
+    }
+}
