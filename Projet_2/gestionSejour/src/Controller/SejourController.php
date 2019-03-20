920f13fab5701292a091c904086c14a7da5d2e44
hichem <hicem>
2019-03-20 11:00:34 +0100

    hichem

--- Projet_2/gestionSejour/src/Controller/SejourController.php
+++ Projet_2/gestionSejour/src/Controller/SejourController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Sejour;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\SejourType;
+use Symfony\Component\HttpFoundation\Request;
 
 class SejourController extends AbstractController
 {
@@ -15,5 +18,26 @@ class SejourController extends AbstractController
         return $this->render('sejour/index.html.twig', [
             'controller_name' => 'SejourController',
         ]);
+    }
+	
+	 /**
+     * @Route("/ajoutSejour", name="ajoutSejour")
+     */
+	 public function creationSejour(Request $request)
+    {
+	 $sejour=new Sejour();
+	 $form=$this->createForm(SejourType::class,$sejour);
+     $form->handleRequest($request);
+	if($form->isSubmitted()&&$form->isValid())
+	{
+	$sejour=$form->getData();
+	$em=$this->getDoctrine()->getManager();
+	$em->persist($sejour);
+	$em->flush();
+	return $this->RedirectToRoute('sejour');
+	}
+	return $this->render('Sejour/formulaireSejour.html.twig', array(
+			'form'=>$form->CreateView(),
+			));  
     }
 }
