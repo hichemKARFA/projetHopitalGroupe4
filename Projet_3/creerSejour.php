22196184b52c66a03f5d5b154c97f2d3da9984b1
Tom Jablonski <tomjablonski@hotmail.fr>
2019-03-20 09:42:57 +0100

    Script appli android

--- Projet_3/creerSejour.php
+++ Projet_3/creerSejour.php
@@ -1,9 +1,8 @@
 <?php
 include_once 'mesFonctions.php';
-$tableau=array('25-02-2019',2,4);
+$tableau=array('2019-03-19',2,4);
 $json= json_encode($tableau);
 $sejour= json_decode($json);
-creerSejour($sejour[0], $sejour[1],$sejour[2])
-
+creerSejour($sejour[0], $sejour[1],$sejour[2]);
 
 ?>
--- Projet_3/mesFonctions.php
+++ Projet_3/mesFonctions.php
@@ -17,7 +17,7 @@ function getPatients()
 function creerSejour($dateArrivee,$idPatient,$idLit)
 {
     $idSejour= attribuerIdMaxSejour();
-    $monObjPdoStatement=getPDO()->prepare("Insert into sejour values (:idSejour,:dateArrivee,null,:idPatient,:idLit)");
+    $monObjPdoStatement=getPDO()->prepare("Insert into sejour values (:idSejour,:dateArrivee ,null,:idPatient,:idLit)");
     $monObjPdoStatement->bindValue(':idSejour', $idSejour);
     $monObjPdoStatement->bindValue(':dateArrivee', $dateArrivee);
     $monObjPdoStatement->bindValue(':idPatient', $idPatient);
@@ -28,15 +28,15 @@ function creerSejour($dateArrivee,$idPatient,$idLit)
 function attribuerIdMaxSejour()
 {
    $id=1;
-   $monObjPdoStatement=getPDO()->prepare("Select count(id) From sejour");
+   $monObjPdoStatement=getPDO()->prepare("Select Count(id) From sejour");
    $okExecution=$monObjPdoStatement->execute();
-   $lesLignesResultatDeLaRequete=$monObjPdoStatement->fetch();
-   if($lesLignesResultatDeLaRequete!=0)
+   $lesLignesResultatDeLaRequete=$monObjPdoStatement->fetch(PDO::FETCH_ASSOC);
+   if($lesLignesResultatDeLaRequete['Count(id)']!=0)
    {
        $monObjPdoStatement=getPDO()->prepare("Select Max(id) From sejour");
        $okExecution=$monObjPdoStatement->execute();
-       $lesLignesResultatDeLaRequete=$monObjPdoStatement->fetch();
-       $id=$lesLignesResultatDeLaRequete+1;
+       $lesLignesResultatDeLaRequete=$monObjPdoStatement->fetch(PDO::FETCH_ASSOC);
+       $id=$lesLignesResultatDeLaRequete['Max(id)']+1;
    }
    return $id;
 }
