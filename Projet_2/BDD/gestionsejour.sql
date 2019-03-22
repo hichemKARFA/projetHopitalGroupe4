f2c744b2578604f453129d3a6d81a14045f456ff
hichem <hicem>
2019-03-22 11:01:20 +0100

    BDDV2

--- Projet_2/BDD/gestionsejour.sql
+++ Projet_2/BDD/gestionsejour.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 15 Mars 2019 à 09:24
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 22 mars 2019 à 10:00
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `gestionsejour`
@@ -28,10 +30,24 @@ USE `gestionsejour`;
 -- Structure de la table `chambre`
 --
 
+DROP TABLE IF EXISTS `chambre`;
 CREATE TABLE IF NOT EXISTS `chambre` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+  `service_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_C509E4FFED5CA9E6` (`service_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `chambre`
+--
+
+INSERT INTO `chambre` (`id`, `service_id`) VALUES
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 2),
+(5, 3);
 
 -- --------------------------------------------------------
 
@@ -39,10 +55,25 @@ CREATE TABLE IF NOT EXISTS `chambre` (
 -- Structure de la table `lit`
 --
 
+DROP TABLE IF EXISTS `lit`;
 CREATE TABLE IF NOT EXISTS `lit` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+  `chambre_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_5DDB8E9D9B177F54` (`chambre_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `lit`
+--
+
+INSERT INTO `lit` (`id`, `chambre_id`) VALUES
+(1, 1),
+(2, 2),
+(3, 2),
+(4, 2),
+(5, 4),
+(6, 5);
 
 -- --------------------------------------------------------
 
@@ -50,6 +81,7 @@ CREATE TABLE IF NOT EXISTS `lit` (
 -- Structure de la table `migration_versions`
 --
 
+DROP TABLE IF EXISTS `migration_versions`;
 CREATE TABLE IF NOT EXISTS `migration_versions` (
   `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
   `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
@@ -57,11 +89,12 @@ CREATE TABLE IF NOT EXISTS `migration_versions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Contenu de la table `migration_versions`
+-- Déchargement des données de la table `migration_versions`
 --
 
 INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
-('20190312153458', '2019-03-12 15:46:50');
+('20190320072542', '2019-03-20 07:26:07'),
+('20190320073734', '2019-03-20 07:38:02');
 
 -- --------------------------------------------------------
 
@@ -69,9 +102,10 @@ INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
 -- Structure de la table `patient`
 --
 
+DROP TABLE IF EXISTS `patient`;
 CREATE TABLE IF NOT EXISTS `patient` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `prenom` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
   `adresse` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -80,15 +114,14 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `telephone` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `mail` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Contenu de la table `patient`
+-- Déchargement des données de la table `patient`
 --
 
 INSERT INTO `patient` (`id`, `nom`, `prenom`, `age`, `adresse`, `ville`, `cp`, `telephone`, `mail`) VALUES
-(1, 'Comptdaer', 'Louis', 20, '6 rue de lys', 'Toufflers', '59390', '0664531995', 'louis.comptdaer@laposte.net'),
-(4, 'Jablonski', 'Tom', 19, '123 rue testMethode', 'Mons-En-Baroeul', '19', '0600000000', 'tom.jablonski@mail.fr');
+(1, 'comptader', 'loulou', 20, '48 rue du petit poney', 'zezez', '57100', '0685987458', 'zezezezeze@mail.fr');
 
 -- --------------------------------------------------------
 
@@ -96,12 +129,27 @@ INSERT INTO `patient` (`id`, `nom`, `prenom`, `age`, `adresse`, `ville`, `cp`, `
 -- Structure de la table `sejour`
 --
 
+DROP TABLE IF EXISTS `sejour`;
 CREATE TABLE IF NOT EXISTS `sejour` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date_arrivee` datetime DEFAULT NULL,
   `date_sortie` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+  `patient_id` int(11) DEFAULT NULL,
+  `lit_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_96F520286B899279` (`patient_id`),
+  KEY `IDX_96F52028278B5057` (`lit_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `sejour`
+--
+
+INSERT INTO `sejour` (`id`, `date_arrivee`, `date_sortie`, `patient_id`, `lit_id`) VALUES
+(1, '2017-05-07 05:04:00', '2021-03-06 03:20:00', NULL, NULL),
+(2, '2016-02-02 06:00:00', '2016-03-03 02:04:00', 1, 1),
+(3, '2019-03-16 00:00:00', '2019-03-24 00:00:00', 1, 3),
+(4, '2018-03-24 00:00:00', '2019-08-15 00:00:00', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -109,11 +157,46 @@ CREATE TABLE IF NOT EXISTS `sejour` (
 -- Structure de la table `service`
 --
 
+DROP TABLE IF EXISTS `service`;
 CREATE TABLE IF NOT EXISTS `service` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `service`
+--
+
+INSERT INTO `service` (`id`, `libelle`) VALUES
+(1, 'Cardiologie'),
+(2, 'Neurologie'),
+(3, 'Pediatrie'),
+(4, 'Rhumatologie');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `chambre`
+--
+ALTER TABLE `chambre`
+  ADD CONSTRAINT `FK_C509E4FFED5CA9E6` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`);
+
+--
+-- Contraintes pour la table `lit`
+--
+ALTER TABLE `lit`
+  ADD CONSTRAINT `FK_5DDB8E9D9B177F54` FOREIGN KEY (`chambre_id`) REFERENCES `chambre` (`id`);
+
+--
+-- Contraintes pour la table `sejour`
+--
+ALTER TABLE `sejour`
+  ADD CONSTRAINT `FK_96F52028278B5057` FOREIGN KEY (`lit_id`) REFERENCES `lit` (`id`),
+  ADD CONSTRAINT `FK_96F520286B899279` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
